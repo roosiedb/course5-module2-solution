bded110f184bf9390ba31ac37e5f1c3e7550f937
Stefan <roosiedb@planet.nl>
2021-01-08 09:12:19 +0100

    update files

--- app.js
+++ app.js
@@ -2,53 +2,73 @@
   'use strict';
 
   var shoppingList = [
-    {name: "cookies", quantity: 10}, {name: "cokies2", quantity: 11}, {name: "cookies3", quantity: 12}
+    {
+      name: "cookies",
+      quantity: "10"
+    },
+    {
+      name: "cookies2",
+      quantity: "11"
+    },
+    {
+      name: "cookies3",
+      quantity: "12"
+    }
   ];
-
-
-
 
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController);
-  .controller('AlreadyBoughtController', AlreadyBoughtController);
-  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+  // .controller('AlreadyBoughtController', AlreadyBoughtController);
+  // .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ShoppingListCheckOffService.$inject['ShoppingListCheckOffService'];
-  function ToBuyController(ShoppingListCheckOffService) {
-    var itemAdder = this;
-      itemAdder.itemName = "";
-      itemAdder.itemQuantity = "";
-
-      itemAdder.addItem = function() {
-      ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
-    };
-  };
-
-  ShoppingListCheckOffService.$inject['ShoppingListCheckOffService'];
-  function AlreadyBoughtController(ShoppingListCheckOffService {
-    var showList = this;
-    showList.items = ShoppingListCheckOffService.getItems();
-  };
-
-  function ShoppingListCheckOffService() {
-    var service = this;
-    var items = [];
-
-    service.addItem = function(itemName, quantity) {
-      var item = {
-        name = itemName,
-        quantity = quantity
-      };
-      items.push(item);
-    };
-
-    service.getItems = function() {
-      return items;
-    };
-  };
+  ToBuyController.$inject = ['$scope'];
+  function ToBuyController($scope) {
+    $scope.shoppingList1 = shoppingList;
+  }
 
 
 
+
+
+
+
+
+  // ShoppingListCheckOffService.$inject['ShoppingListCheckOffService'];
+  // function ToBuyController(ShoppingListCheckOffService) {
+  //   var itemAdder = this;
+  //     itemAdder.itemName = "";
+  //     itemAdder.itemQuantity = "";
+  //
+  //     itemAdder.addItem = function() {
+  //     ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  //   };
+  // };
+  //
+  // ShoppingListCheckOffService.$inject['ShoppingListCheckOffService'];
+  // function AlreadyBoughtController(ShoppingListCheckOffService {
+  //   var showList = this;
+  //   showList.items = ShoppingListCheckOffService.getItems();
+  // };
+  //
+  // function ShoppingListCheckOffService() {
+  //   var service = this;
+  //   var items = [];
+  //
+  //   service.addItem = function(itemName, quantity) {
+  //     var item = {
+  //       name = itemName,
+  //       quantity = quantity
+  //     };
+  //     items.push(item);
+  //   };
+  //
+  //   service.getItems = function() {
+  //     return items;
+  //   };
+  // };
+  //
+  //
+  //
 
 
 
